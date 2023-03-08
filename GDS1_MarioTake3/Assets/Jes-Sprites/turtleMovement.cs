90d630177311c2a54f6a2ad47339b5c856dd075c
Jesica Nikolovska <jesica.nikolovska@student.uts.edu.au>
2023-03-08 20:20:21 +1100

    Collision for turtle done

---
+++ GDS1_MarioTake3/Assets/Jes-Sprites/turtleMovement.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turtleMovement : MonoBehaviour
+{
+    public Rigidbody2D enemy;
+    public Renderer rend;
+    public float enemySpeed;
+    public Animator animationControl;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemy = gameObject.GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (rend.isVisible)
+        {
+
+            animationControl.SetTrigger("TurtleRight");
+            enemy.velocity = gameObject.transform.right * enemySpeed;
+
+
+
+        }
+
+
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "pip1")
+        {
+            animationControl.SetTrigger("TurtleLeft");
+            enemy.transform.Rotate(0f, 180f, 0);
+
+
+        }
+        if (collision.gameObject.tag == "pip-reg2")
+        {
+            animationControl.SetTrigger("TurtleRight");
+            enemy.transform.Rotate(0f, 180f, 0);
+
+        }
+
+        if (collision.gameObject.tag == "Player")
+        {
+            animationControl.SetTrigger("TurtleDeath");
+            animationControl.SetTrigger("Shell");
+            enemy.transform.Rotate(0f, 180f, 0);
+           
+
+        }
+
+
+
+    }
+}
