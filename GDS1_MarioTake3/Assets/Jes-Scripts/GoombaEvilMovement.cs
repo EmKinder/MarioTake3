d14f69a06e40266cbde9559b2aa7157960f231de
Jesica Nikolovska <jesica.nikolovska@student.uts.edu.au>
2023-03-08 21:39:03 +1100

    GoombaEvilMovement

---
+++ GDS1_MarioTake3/Assets/Jes-Scripts/GoombaEvilMovement.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoombaEvilMovement : MonoBehaviour
+{
+    public Rigidbody2D enemy;
+    public SpriteRenderer rend;
+    public float enemySpeed;
+    public Animator animationControl;
+    public GameObject objection;
+    public bool forward;
+    public PolygonCollider2D col;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemy = gameObject.GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (rend.isVisible)
+        {
+
+            animationControl.SetTrigger("GULeft");
+            enemy.velocity = -gameObject.transform.right * enemySpeed;
+
+
+
+        }
+
+
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (rend.isVisible)
+        {
+            if (collision.gameObject.tag == "pip1")
+            {
+                animationControl.SetTrigger("GURight");
+                enemy.transform.Rotate(0f, 180f, 0);
+
+            }
+            if (collision.gameObject.tag == "pip-reg2")
+            {
+                animationControl.SetTrigger("GULeft");
+                enemy.transform.Rotate(0f, 180f, 0);
+            }
+            if (collision.gameObject.tag == "Player")
+            {
+                animationControl.SetTrigger("GUDeath");
+                rend.enabled = !rend.enabled;
+                col.enabled = !col.enabled;
+                // enemy.transform.Rotate(0f, 180f, 0);
+
+            }
+        }
+    }
+    }
